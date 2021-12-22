d3da979e9dc537152bab6856986921f0e575297f
luis leon <leonluis380@yahoo.com>
2021-12-22 14:05:19 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,12 @@
 class Application
 
+  @@NUM = 0
+
 
 
     def call(env)
+
+
       resp = Rack::Response.new
   
       num_1 = Kernel.rand(1..20)
@@ -10,12 +14,14 @@ class Application
       num_3 = Kernel.rand(1..20)
   
       if num_1==num_2 && num_2==num_3
-        resp.write "You Win"
+        resp.write "You Win "
       else
         resp.write "You Lose \n"
-        resp.write "numbers = #{num_1}_ #{num_2}_#{num_3} "
+        resp.write "numbers = #{num_1}_ #{num_2}_#{num_3}\n  "
+        resp.write "#{@@NUM /2}"
+        
       end
-  
+      @@NUM +=1
       resp.finish
     end
   
